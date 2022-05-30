440ef40c3b6a48547d509225c0bf65bd30ab9e33
ShaimaaElsayed98 <53016795+ShaimaaElsayed98@users.noreply.github.com>
2022-05-30 17:53:27 +0300

    Update Routes.js

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,1 +1,37 @@
+var express = require("express");
+var router = express.Router();
 
+
+
+
+
+/*render pages*/
+router.get("/", function(req,res){
+    res.render("index")
+});
+
+router.get("/Programs", function(req,res){
+    res.render("Programs")
+});
+
+router.get("/Proj", function(req,res){
+    res.render("Proj")
+});
+
+router.get("/Exec", function(req,res){
+    res.render("Exec")
+});
+
+router.get("/Org", function(req,res){
+    res.render("Org")
+});
+
+router.get("/Researchers", function(req,res){
+    res.render("Researchers")
+});
+
+router.get("/Sci_Fields", function(req,res){
+    res.render("Sci_Fields")
+});
+
+module.exports = router;
