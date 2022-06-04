b392b72650b3fd7e53008de946454057ea00fa24
ShaimaaElsayed98 <53016795+ShaimaaElsayed98@users.noreply.github.com>
2022-06-04 05:54:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,73 +1,67 @@
-var express = require("express");
-var path = require("path");
-const mysql=require('mysql')
-var routes= require("./Routes/routes");
-const { request } = require("http");
-const console = require("console");
-var app =  express();
-
-
-/*Database connection through pool (not working)
-/*const pool = mysql.createPool({
-    connectionLimit : 100,
-    host : process.env.DB_HOST,
-    user : process.env_DB_USER,
-    database : process.env.DB_NAME,
-    port : process.env.DB_PORT
-})*/
-
-
-/*database connected*/
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "elidek",
-    port: "3306"
-})
-
-/*Not working
-/*pool.getConnection((err, connection) => {
-    if(err){
-        throw err
-    }
-    else {
-        console.log("connected")
-    }
-});*/
-
-connection.connect((err) =>{
-    if(err){
-        throw err
-    }
-    else {
-        console.log("database connected")
-    }
-
-})
-
-/*Tested the connection, it works
-connection.connect(function(err){
-    if(err)throw err;
-    connection.query("SELECT * from programs",function(err,result){
-          if(err)throw err;
-          console.log(result);
-
-    })
-})*/
+const express = require('express');
+const exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const path = require('path');
+const mysql = require('mysql2');
+const req = require('express/lib/request');
 
 
 
+require('dotenv').config();
 
-/*connected through port 3000*/
-app.set("port", process.env.PORT || 3000);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(flash());
+app.use(session({
+    secret: "ThisShouldBeSecret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+//static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+//Templating Engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '/public'));
-app.use(routes);
 
-app.listen(app.get("port"),function(){
-    console.log("Server started on port " + app.get("port"));
+//Connection Pool
+const pool = mysql.createPool({
+    connectionLimit : 100, 
+    host            : process.env.DB_HOST,
+    user            : process.env.DB_USER,
+    password        : process.env.DB_PASS,   
+    database        : process.env.DB_NAME,
+    port            : process.env.DB_PORT 
 });
 
+//connect to DB
+pool.getConnection((err, connection) =>{
+    if(err) throw err; //not connected
+    console.log('database connected');
+});
+
+const routes = require('./server/routes/ques3');
+app.use('/',routes);
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+app.listen(port,() => console.log(`Listening on port ${port}`));
+
+
+module.exports= app;
